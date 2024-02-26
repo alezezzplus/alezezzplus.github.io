ce612a4fe54111e86d1c5224b0eeebb7d38b6cbc
alezezzplus <161356490+alezezzplus@users.noreply.github.com>
2024-02-26 11:01:51 -0600

    Update indexscript.js

--- indexscript.js
+++ indexscript.js
@@ -1,1 +1,20 @@
+window.document.querySelector('.menu-btn').addEventListener('click', function () {
+  window.document.querySelector('.main-nav-menu').classList.toggle('show');
+});
 
+
+
+
+document.addEventListener("contextmenu", function (e) {
+    e.preventDefault();
+
+    var contextMenu = document.getElementById("contextMenu");
+
+    contextMenu.style.display = "block";
+    contextMenu.style.left = e.clientX + "px";
+    contextMenu.style.top = e.clientY + "px";
+
+    document.addEventListener("click", function () {
+        contextMenu.style.display = "none";
+    }, {once: true});
+});
